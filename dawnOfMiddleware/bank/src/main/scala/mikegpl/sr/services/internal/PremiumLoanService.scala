98b3af45da0fedfec0d04a5c2df0b035d6e21099
Michał Grabowski <mkg.grabowski@gmail.com>
2018-06-04 20:47:05 +0200

    [Lab4] Finished impl of LoanService.

--- dawnOfMiddleware/bank/src/main/scala/mikegpl/sr/services/internal/PremiumLoanService.scala
+++ dawnOfMiddleware/bank/src/main/scala/mikegpl/sr/services/internal/PremiumLoanService.scala
@@ -1,14 +1,23 @@
 package mikegpl.sr.services.internal
 
-import mikegpl.sr.services.{BankUtils, LoanService}
-import mikegpl.sr.thrift.{InvalidAccountTypeException, LoanOffer}
+import mikegpl.sr.services.{BankUtils, CurrencyService, LoanService}
+import mikegpl.sr.thrift.{InvalidAccountTypeException, LoanInquiry, LoanOffer}
 
-class PremiumLoanService(dbService: SynchronizedDbService) extends LoanService {
-  def loanConditions(guid: Long): LoanOffer = {
-    val client = dbService.getClient(guid)
+class PremiumLoanService(dbService: SynchronizedDbService, currencyService: CurrencyService) extends LoanService {
+  def loanConditions(inquiry: LoanInquiry): LoanOffer = {
+    val client = dbService.getClient(inquiry.guid)
     if (!BankUtils.isEligibleForLoan(client))
-      throw new InvalidAccountTypeException(guid, s"Invalid account type: ${client.`type`}")
+      throw new InvalidAccountTypeException(inquiry.guid, s"Invalid account type: ${client.`type`}")
 
-    new LoanOffer(200, 100)
+    val conversionRate = currencyService.getConversionRate(inquiry.currency, client.owner.baseCurrency)
+    val baseCost = loanCost(inquiry.amount, conversionRate)
+    val requestedCost = loanCost(inquiry.amount)
+    new LoanOffer(baseCost, requestedCost)
   }
+
+  override protected def loanCost(amount: Double, conversionRate: Double = 1.0): Double = amount * conversionRate * PremiumLoanService.LoanCost
+}
+
+object PremiumLoanService {
+  private val LoanCost = 0.2
 }
--- dawnOfMiddleware/bank/src/main/scala/mikegpl/sr/services/remote/PremiumAccountManagerService.scala
+++ dawnOfMiddleware/bank/src/main/scala/mikegpl/sr/services/remote/PremiumAccountManagerService.scala
@@ -5,7 +5,5 @@ import mikegpl.sr.thrift.{LoanInquiry, LoanOffer, PremiumAccountManager}
 
 class PremiumAccountManagerService(dbService: DbService, loanService: LoanService)
   extends StandardAccountManagerService(dbService) with PremiumAccountManager.Iface {
-  override def getLoanConditions(inquiry: LoanInquiry): LoanOffer = {
-    loanService.loanConditions(inquiry.guid)
-  }
+  override def getLoanConditions(inquiry: LoanInquiry): LoanOffer = loanService.loanConditions(inquiry)
 }
